298a2cee6807bfbe6b0cb2bd01a4fff7168528f3
Lewis Purvis <77275854+LSPgh@users.noreply.github.com>
2025-01-23 17:08:45 +0000

    change logic so that bl is only overwritten if u-boot env var exists an… (#47)
    * change logic so that bl is only ovwritten if u-boot env var exists and is set to correct value. Clear flag once executed.

--- recipes-support/swupdate/imdt-image-core-swu/update.sh
+++ recipes-support/swupdate/imdt-image-core-swu/update.sh
@@ -25,7 +25,33 @@ function get_update_part
 
 function get_update_device
 {
-    	UPDATE_BOOT_DEV=${CURRENT_ROOT%?}${UPDATE_BOOT_PART}
+    UPDATE_BOOT_DEV=${CURRENT_ROOT%?}${UPDATE_BOOT_PART}
+}
+
+function write_bootloader
+{
+	# write boot img
+	echo "This will overwrite the current bootloader! Changes CANNOT be reverted!"
+	echo "${PWD}"
+
+	# if current_root is sdcard, flash the QSPI with new bootloaders
+	if [ "/dev/mmcblk1p2" == $CURRENT_ROOT ] || [ "/dev/mmcblk1p3" == $CURRENT_ROOT ]; then
+		echo SD
+		dd if=/dev/zero of=boot.img bs=1024 count=1024 
+		dd if=/tmp/bl2_bp_spi-imdt-v2h-sbc.bin of=boot.img  conv=notrunc 
+		dd if=/tmp/fip-imdt-v2h-sbc.bin  of=boot.img conv=notrunc bs=512 seek=768 
+		flashcp -v boot.img /dev/mtd0 
+	fi
+
+	# if current_root is emmc, flash the emmc with new bootloaders
+	if [ "/dev/mmcblk0p2" == $CURRENT_ROOT ] || [ "/dev/mmcblk0p3" == $CURRENT_ROOT ]; then
+		echo EMMC
+		mmc bootpart enable 1 0 /dev/mmcblk0  # enable the first boot-area 0 for boot, no ack
+		mmc bootbus set single_backward x1 x8 /dev/mmcblk0  # setup the boot bus config
+		echo 0 > /sys/block/mmcblk0boot0/force_ro
+		dd if=/tmp/bl2_bp_emmc-imdt-v2h-sbc.bin of=/dev/mmcblk0boot0 bs=512 skip=0 seek=1 >/dev/null 2>/dev/null
+		dd if=/tmp/fip-imdt-v2h-sbc.bin of=/dev/mmcblk0boot0 bs=512 skip=0 seek=768 >/dev/null 2>/dev/null
+	fi
 }
 
 if [ $1 == "preinst" ]; then
@@ -49,30 +75,26 @@ if [ $1 == "preinst" ]; then
 	# create symlinks for the update process
     ln -sf $UPDATE_BOOT_DEV /dev/update
 
-	# write boot img
-	echo "This will overwrite the current bootloader! Changes CANNOT be reverted!"
-	echo "${PWD}"
+	# Get the output of fw_printenv
+	env_output=$(fw_printenv)
 
-	# if current_root is sdcard, flash the QSPI with new bootloaders
-	if [ "/dev/mmcblk1p2" == $CURRENT_ROOT ] || [ "/dev/mmcblk1p3" == $CURRENT_ROOT ]; then
-		echo SD
-		dd if=/dev/zero of=boot.img bs=1024 count=1024 
-		dd if=/tmp/bl2_bp_spi-imdt-v2h-sbc.bin of=boot.img  conv=notrunc 
-		dd if=/tmp/fip-imdt-v2h-sbc.bin  of=boot.img conv=notrunc bs=512 seek=768 
-		flashcp -v boot.img /dev/mtd0 
+	# Search for the line containing overwrite_bl
+	overwrite_bl_line=$(echo "$env_output" | grep '^overwrite_bl=')
+
+	# Check if the line exists
+	if [ -n "$overwrite_bl_line" ]; then
+		# Extract the value after '='
+		overwrite_bl_value=$(echo "$overwrite_bl_line" | cut -d'=' -f2)
+
+		# Check if the value is 1
+		if [ "$overwrite_bl_value" -eq 1 ]; then
+			echo "overwrite_bl is set to 1. Flashing bootloader..."
+			write_bootloader
+			# reset the env variable back to zero
+			echo "clearing bootloader overwrite flag..."
+			fw_setenv "overwrite_bl" "0"
+		fi
 	fi
-
-
-	# if current_root is emmc, flash the QSPI with new bootloaders
-	if [ "/dev/mmcblk0p2" == $CURRENT_ROOT ] || [ "/dev/mmcblk0p3" == $CURRENT_ROOT ]; then
-		echo EMMC
-		mmc bootpart enable 1 0 /dev/mmcblk0  # enable the first boot-area 0 for boot, no ack
-		mmc bootbus set single_backward x1 x8 /dev/mmcblk0  # setup the boot bus config
-		echo 0 > /sys/block/mmcblk0boot0/force_ro
-		dd if=/tmp/bl2_bp_emmc-imdt-v2h-sbc.bin of=/dev/mmcblk0boot0 bs=512 skip=0 seek=1 >/dev/null 2>/dev/null
-		dd if=/tmp/fip-imdt-v2h-sbc.bin of=/dev/mmcblk0boot0 bs=512 skip=0 seek=768 >/dev/null 2>/dev/null
-	fi
-
 fi
 
 if [ $1 == "postinst" ]; then
